89b8973e86f8fdef0111f6e64e8d5088f6eef978
unknown <thepancakeface823@gmail.com>
2025-02-20 20:01:03 -0300

    update2

--- nutriapp/app/Http/Controllers/Auth/GoogleController.php
+++ nutriapp/app/Http/Controllers/Auth/GoogleController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
-use Illuminate\Support\Str; // Importe a classe Str
+use Illuminate\Support\Str;
 
 class GoogleController extends Controller
 {
@@ -19,22 +19,37 @@ class GoogleController extends Controller
     {
         try {
             $googleUser = Socialite::driver('google')->user();
-
-            $user = User::updateOrCreate([
-                'google_id' => $googleUser->getId(),
-            ], [
-                'name' => $googleUser->getName(),
-                'email' => $googleUser->getEmail(),
-                'password' => Str::random(20), // Gere uma senha aleatória
-                'google_token' => $googleUser->token,
-                'google_refresh_token' => $googleUser->refreshToken,
-            ]);
+            
+            // Procura um usuário pelo email do Google
+            $existingUser = User::where('email', $googleUser->getEmail())->first();
+            
+            if ($existingUser) {
+                // Se o usuário existe, atualiza as informações do Google
+                $existingUser->update([
+                    'google_id' => $googleUser->getId(),
+                    'google_token' => $googleUser->token,
+                    'google_refresh_token' => $googleUser->refreshToken,
+                ]);
+                
+                $user = $existingUser;
+            } else {
+                // Se o usuário não existe, cria um novo
+                $user = User::create([
+                    'name' => $googleUser->getName(),
+                    'email' => $googleUser->getEmail(),
+                    'password' => Str::random(20),
+                    'google_id' => $googleUser->getId(),
+                    'google_token' => $googleUser->token,
+                    'google_refresh_token' => $googleUser->refreshToken,
+                ]);
+            }
 
             Auth::login($user);
 
             return redirect()->intended('/dashboard');
-        } catch (\Throwable $th) {
-            dd('Ocorreu um erro ao fazer login com o Google: ' . $th->getMessage());
+        } catch (\Exception $e) {
+            return redirect()->route('login')
+                ->with('error', 'Ocorreu um erro ao fazer login com o Google: ' . $e->getMessage());
         }
     }
 }
